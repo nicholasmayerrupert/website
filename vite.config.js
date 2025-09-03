6681be0f27a185e3255dfaa9a745fe8045427c4a
Nicholas Mayer-Rupert <njmrme@gmail.com>
2025-09-02 21:57:48 -0700

    site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website/'
+  base: '/website/',      // project page under /website
+  build: { outDir: 'docs' } // put build in ./docs
 })

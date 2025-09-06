f190bfaa8a260ac2c4a94316484e8987dfda4339
Nicholas Mayer-Rupert <njmrme@gmail.com>
2025-09-05 21:38:59 -0700

    fixed for cloudflare

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website/',      // project page under /website
+  base: '/',      // project page under /website
   build: { outDir: 'dist' } // put build in ./docs
 })

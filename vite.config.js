eb6d0b698c4087a5010f844071822dfcca1d3025
Nicholas Mayer-Rupert <njmrme@gmail.com>
2026-06-10 21:12:31 -0700

    cleaned up repo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',      // project page under /website
-  build: { outDir: 'dist' } // put build in ./docs
+  base: '/',
+  build: { outDir: 'dist' }
 })

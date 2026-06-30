611333af31cea7f201e3ddadadc83d13a4af87e9
Nicholas Mayer-Rupert <njmrme@gmail.com>
2026-06-29 20:36:29 -0700

    hero language change

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="relative flex h-[100dvh] flex-col md:flex-row overflow-hidden bg-[#222222]">
       {/* Text block (top-most) */}
-      <div className="relative z-20 order-1 md:order-1 w-full md:w-1/2 lg:w-1/3 text-white px-6 sm:px-12 md:pl-24 pt-16 sm:pt-20 md:pt-28">
+      <div className="relative z-20 order-1 md:order-1 w-full md:w-1/2 lg:w-1/3 text-white px-6 sm:px-12 md:pl-24 pt-16 sm:pt-20 md:pt-36 lg:pt-40">
         <SplitText
           text="NICHOLAS MAYER-RUPERT"
           className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-4 md:mb-6 leading-tight md:leading-none"
@@ -13,15 +13,6 @@ const Hero = () => {
           textAlign="left"
           splitType="words,chars"
           delay={30}
-          duration={0.3}
-        />
-        <SplitText
-          text="COMPUTER SCIENCE STUDENT AT SFU. SOFTWARE ENGINEERING ENJOYER."
-          className="text-xl sm:text-2xl md:text-3xl md:ml-16"
-          tag="p"
-          textAlign="left"
-          splitType="words,chars"
-          delay={20}
           duration={0.3}
         />
       </div>
